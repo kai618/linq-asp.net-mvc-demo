ece429b7eb301415d2a17c646633c7ed87da9358
kai618 <vanhieu1601@gmail.com>
2020-04-13 15:21:07 +0700

    after presentation and demo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,55 @@
 ﻿using System.Web.Mvc;
 using System.Linq;
+using LinqDemoApp.Models;
 
 namespace LinqDemoApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly eshopEntities _context = new eshopEntities();
+
         // select : GET
         public ActionResult Index()
         {
-            return View();
+            var types = (from loai in _context.LoaiSanPhams where loai.MaLoaiSP < 3 select loai).ToList();
+
+            var typesL = _context.LoaiSanPhams.ToList();
+
+            return View(typesL);
         }
 
         // insert : POST
+        public void Insert(string value)
+        {
+            _context.LoaiSanPhams.Add(new LoaiSanPham
+            {
+                TenLoaiSP = value
+            });
+
+            _context.SaveChanges();
+        }
+
 
         // update : POST
+        public void Update(int id, string value)
+        {
+            var type = _context.LoaiSanPhams.SingleOrDefault(t => t.MaLoaiSP == id);
+            if (type == null) return;
+
+            type.TenLoaiSP = value;
+            _context.SaveChanges();
+        }
 
         // delete : POST
+
+        public void Delete(int id)
+        {
+            var type = _context.LoaiSanPhams.SingleOrDefault(t => t.MaLoaiSP == id);
+            if (type == null) return;
+
+            _context.LoaiSanPhams.Remove(type);
+            _context.SaveChanges();
+        }
+
     }
 }
--- Controllers/ShoeController.cs
+++ Controllers/ShoeController.cs
@@ -1,59 +1,59 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using LinqDemoApp.Models;
+﻿//using System.Linq;
+//using System.Web.Mvc;
+//using LinqDemoApp.Models;
 
-namespace LinqDemoApp.Controllers
-{
-    public class ShoeController : Controller
-    {
-        private readonly eshopEntities _db = new eshopEntities();
-
-
-        // select : GET
-        [HttpGet]
-        public ActionResult Index()
-        {
-            var typeListLambda = _db.LoaiSanPhams.ToList();
-
-            return View(typeListLambda);
-        }
-
-        // insert : POST
-        [HttpPost]
-        public void Add(string type)
-        {
-            _db.LoaiSanPhams.Add(new LoaiSanPham
-                {
-                    TenLoaiSP = type
-                }
-            );
-
-            _db.SaveChanges();
-        }
+//namespace LinqDemoApp.Controllers
+//{
+//    public class ShoeController : Controller
+//    {
+//        private readonly eshopEntities _db = new eshopEntities();
 
 
-        // update : POST
-        [HttpPost]
-        public void Update(int id, string value)
-        {
-            var type = _db.LoaiSanPhams.SingleOrDefault(t => t.MaLoaiSP == id);
-            if (type == null) return;
+//        // select : GET
+//        [HttpGet]
+//        public ActionResult Index()
+//        {
+//            var typeListLambda = _db.LoaiSanPhams.ToList();
 
-            type.TenLoaiSP = value;
+//            return View(typeListLambda);
+//        }
 
-            _db.SaveChanges();
-        }
+//        // insert : POST
+//        [HttpPost]
+//        public void Add(string type)
+//        {
+//            _db.LoaiSanPhams.Add(new LoaiSanPham
+//                {
+//                    TenLoaiSP = type
+//                }
+//            );
+
+//            _db.SaveChanges();
+//        }
 
 
-        // delete : POST
-        [HttpPost]
-        public void Delete(int id)
-        {
-            var type = _db.LoaiSanPhams.SingleOrDefault(t => t.MaLoaiSP == id);
-            if (type == null) return;
+//        // update : POST
+//        [HttpPost]
+//        public void Update(int id, string value)
+//        {
+//            var type = _db.LoaiSanPhams.SingleOrDefault(t => t.MaLoaiSP == id);
+//            if (type == null) return;
 
-            _db.LoaiSanPhams.Remove(type);
-            _db.SaveChanges();
-        }
-    }
-}
+//            type.TenLoaiSP = value;
+
+//            _db.SaveChanges();
+//        }
+
+
+//        // delete : POST
+//        [HttpPost]
+//        public void Delete(int id)
+//        {
+//            var type = _db.LoaiSanPhams.SingleOrDefault(t => t.MaLoaiSP == id);
+//            if (type == null) return;
+
+//            _db.LoaiSanPhams.Remove(type);
+//            _db.SaveChanges();
+//        }
+//    }
+//}
